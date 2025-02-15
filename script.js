13baff226b4f66762358cd7dcf0e2f4beaf40808
Hotdog <louisarmanaceron2@gmail.com>
2025-02-15 12:34:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -93,12 +93,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-fetch("https://api64.ipify.org?format=json")
-  .then(res => res.json())
-  .then(data => {
-    fetch("https://script.google.com/macros/s/AKfycbzgVQg9rxLe4Tzj77-mmqI5SMIJqt5zIbL6SfbkbT-JxHDkj1TXUz7Pl3GHBfujDyqlJw/exec?ip=" 
-      + encodeURIComponent(data.ip) + "&ua=" + encodeURIComponent(navigator.userAgent))
-      .then(response => console.log("Visit recorded"))
-      .catch(error => console.error("Error recording visit:", error));
-  })
-  .catch(error => console.error("Error fetching IP:", error));
+fetch("https://script.google.com/macros/s/AKfycbybihXP8FEuu_wEWhBcvlu5fdcWK5kPlAgwVosXV8SPa5fYB724DrPApDYUI26KCZAuFw/exec?ua=" + encodeURIComponent(navigator.userAgent))
+  .then(response => console.log("Visit recorded"))
+  .catch(error => console.error("Error:", error));
